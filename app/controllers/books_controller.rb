ee29243b3583835fa09ec17d40488c0395f6052f
EC2 Default User <104750358+morunc4@users.noreply.github.com>
2022-05-26 09:46:45 +0000

    [finish]app課題

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,9 +26,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book=Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book=Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id),notice: 'Book was successfully updated'
+    else
+      @books=Book.all
+      render :edit
+    end
   end
 
   def destroy
