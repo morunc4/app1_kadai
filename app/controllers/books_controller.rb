99d27cea925b96e9eb4c8af5b5e1d095b80588ad
EC2 Default User <104750358+morunc4@users.noreply.github.com>
2022-05-26 08:32:54 +0000

    5/26 omari

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,40 @@
 class BooksController < ApplicationController
   def index
     @book =Book.new
+    @books=Book.all
   end
 
   def create
-    book= Book.new(book_params)
-    book.save
-    redirect_to '/books/show'
+    @book= Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id) ,notice: 'Book was successfully created.'
+    else
+      @books=Book.all
+      render :index
+    end
+
+
   end
 
   def show
+    @books=Book.all
+    @book=Book.find(params[:id])
   end
 
   def edit
+    @book=Book.find(params[:id])
+  end
+
+  def update
+    book=Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book=Book.find(params[:id])
+    book.destroy
+    redirect_to '/books',notice: 'Book was successfully destroyed'
   end
 
   private
